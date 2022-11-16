c55fa722f7b0caa414a6c7b0caa4f20b153eecba
AssiniP <paulaassini04@gmail.com>
2022-11-16 02:59:35 -0300

    Carrito andando, valid tarjeta separada de carrito

--- TpIntegradorWeb/js/carrito.js
+++ TpIntegradorWeb/js/carrito.js
@@ -5,160 +5,112 @@ var dataCars = localStorage.getItem("dataCars");//carga los datos del stored dat
 
 dataCars = JSON.parse(dataCars); //convierte el string en objeto
 
-if(dataCars == null) //si no es data, inicializa el array vacio
-dataCars = [];
+if (dataCars == null) //si no es data, inicializa el array vacio
+	dataCars = [];
 
 
 
-function ListCar(){
-	document.getElementById('tblList').innerHTML ="";
-	var datos =" ";
-    //datos += "<table>" ;
+function ListCar() {
+	//document.getElementById('tblList').innerHTML = "";
+	var datos = " ";
+	//datos += "<table>" ;
+	//datos += "<colgroup> <col span=""3""> <col span= ""1"" style=""border: none""> </colgroup>";
 	datos += "<thead>";
-	datos +=	"<tr>";
-	datos +=	"	<th>Producto</th>";
-	datos +=	"	<th>Cantidad</th>";
-	datos +=	"	<th>Precio</th>";	
-	datos +=	"	<th></th>";	
-	datos +=	"</tr>";
-	datos +="</thead>";
-	datos +="<tbody>";
-		for(var i in dataCars){
-			var cli = JSON.parse(dataCars[i]);
-		  	datos +="<tr>";
+	datos += "<tr>";
+	datos += "	<th>Producto</th>";
+	datos += "	<th>Cantidad</th>";
+	datos += "	<th>Precio</th>";
+	datos += "	<th></th>";
+	datos += "</tr>";
+	datos += "</thead>";
+	datos += "<tbody>";
+	for (var i in dataCars) {
+		var cli = JSON.parse(dataCars[i]);
+		datos += "<tr>";
 
-			datos += "	<td>"+cli.Producto+"</td>" ;
-			datos += "	<td>"+cli.Cantidad+"</td>" ;
-			datos += "	<td>"+cli.Precio+"</td>" ;
-			datos += ' <td><span class="icon-bin" alt="Delete" class="btnDelete"  onClick="DeleteCar(\''+i+'\');"/> </td>' ;
-			datos += "</tr>";
-			
-		}
-		datos +="</tbody>";
-		//datos += "</table>";
-	document.getElementById('tblList').innerHTML =datos;
+		datos += "	<td>" + cli.Producto + "</td>";
+		datos += "	<td>" + cli.Cantidad + "</td>";
+		datos += "	<td>" + cli.Precio + "</td>";
+		datos += ' <td><span class="icon-bin" alt="Delete" class="btnDelete"  onClick="DeleteCar(\'' + i + '\');"/> </td>';
+		datos += "</tr>";
+
+	}
+	datos += "</tbody>";
+	//datos += "</table>";
+	document.getElementById('tblList').innerHTML = datos;
 	cargarContadorCarrito();
 
 	console.log('entro en el ListCarar')
-	
-	}
 
-function cargarContadorCarrito(){
-	document.getElementById('contadorCarrito').innerHTML=dataCars.length;
 }
 
-function AddCar(producto,cantidad,precio){
-	var car = JSON.stringify({
-	Producto    : producto,
-	Cantidad  : cantidad ,
-	Precio : precio });
-    dataCars.push(car);
-    localStorage.setItem("dataCars", JSON.stringify(dataCars));
-	cargarContadorCarrito();
-	ListCar();	
-	return true;
-	}
+function cargarContadorCarrito() {
+	document.getElementById('contadorCarrito').innerHTML = dataCars.length;
+}
 
-function DeleteCar(selected_index){
+
+function AddCar(producto, cantidad, precio) {
+	let cantidadDelProducto = document.getElementById(String(producto)).value;
+	console.log(cantidadDelProducto)
+	if (cantidadDelProducto > 99) {
+		cantidadDelProducto = 99;
+		document.getElementById(String(producto)).value = 99;
+	}
+	let precioTotal = parseInt(precio) * cantidadDelProducto;
+	console.log(precioTotal)
+
+	var car = JSON.stringify({
+		Producto: producto,
+		Cantidad: cantidadDelProducto,
+		Precio: "$" + precioTotal
+	});
+
+	dataCars.push(car);
+	localStorage.setItem("dataCars", JSON.stringify(dataCars));
+	cargarContadorCarrito();
+	ListCar();
+	return true;
+}
+
+function DeleteCar(selected_index) {
 	dataCars.splice(selected_index, 1);
 	localStorage.setItem("dataCars", JSON.stringify(dataCars));
 	ListCar();
-	}
+}
 
-function GuardarCar(){
-	selected_index=document.getElementById("txtOperacion").value ;
-		if(selected_index == "A")
-			return AddCar();
-		else '' ;
-	}
+function GuardarCar() {
+	selected_index = document.getElementById("txtOperacion").value;
+	if (selected_index == "A")
+		return AddCar();
+	else '';
+}
 
-function RemoveAllDataCar(){
-	for(var i in dataCars){
-		dataCars.splice(i-1, 1);
+function RemoveAllDataCar() {
+	for (var i in dataCars) {
+		dataCars.splice(i - 1, 1);
 	}
 
 }
 
-function pago(){
-	let setPopUpTarjeta=document.querySelector(".popup-form")
-    estadoDelPopup(setPopUpTarjeta)
+function pago() {
+	let setPopUpTarjeta = document.querySelector(".popup-form")
+	estadoDelPopup(setPopUpTarjeta)
 }
 
-function estadoDelPopup(popup){
-    if(popup.classList.contains("oculto")){
-        popup.classList.remove("oculto")
-    }else{
-        popup.classList.add("oculto")
-    }
+function estadoDelPopup(popup) {
+	if (popup.classList.contains("oculto")) {
+		popup.classList.remove("oculto")
+	} else {
+		popup.classList.add("oculto")
+	}
 }
 
 //tarjeta
 const checkboxTarjeta = document.getElementById("checkTarjeta")
 console.log(checkboxTarjeta)
 
-checkboxTarjeta.addEventListener("change", (e)=>{
+checkboxTarjeta.addEventListener("change", (e) => {
 	let nuevaTarjeta = document.querySelector(".nueva-tarjeta")
 	console.log(nuevaTarjeta)
 	estadoDelPopup(nuevaTarjeta);
 })
-
-const regexNroTarjeta = /^[0-9]{10}$/;
-
-const formulario = document.querySelector(".formularioLogin")
-console.log(formulario)
-
-formulario.addEventListener("submit", (e)=>{
-	e.preventDefault();
-	validarCamposDelFormulario();
-})
-
-//copyPaste de validacionTarjeta
-
-function validarCamposDelFormulario(){
-    let error = false;
-    let mensajeDeError = "";
-    let numeroDeTarjeta = document.querySelector("#nroTarjeta").value;
-    let ultimoDigito = numeroDeTarjeta.charAt(9);
-
-    if(!regexNroTarjeta.test(numeroDeTarjeta)){
-        error = true;
-        mensajeDeError += "<p> La tarjeta debe tener 10 digitos </p>"
-    }
-
-    let resultadoDeLaSumatoria = calcularSiLaSumatoriaDeNumerosEsParOImpar(numeroDeTarjeta);
-    let resultadoDelUltimoNumero = calcularSiEsUnNumeroParOImpar(ultimoDigito);
-
-    if(resultadoDeLaSumatoria == resultadoDelUltimoNumero){
-        error = true;
-        mensajeDeError += "<br><p> Numero de tarjeta inválida</p>"
-    }
-    
-    if(numeroDeTarjeta == "0000000000"){
-        error = true;
-        mensajeDeError += "<br><p> Numero de tarjeta no puede ser 0000000000</p>"
-    }
-
-    if(error){
-        document.querySelector(".mensajeError").innerHTML = mensajeDeError;
-    } else{
-        formulario.submit();
-        AddPago(aliasDeTarjeta);
-        ListPago();
-        mostrarTarjeta();
-    }
-}
-
-function calcularSiLaSumatoriaDeNumerosEsParOImpar(numeroDeTarjeta){
-    let sumatoria = 0;
-    for(let i = 0; i < numeroDeTarjeta.length; i++){
-        sumatoria += parseInt(numeroDeTarjeta.charAt(i))
-    }
-    let resultado = calcularSiEsUnNumeroParOImpar(sumatoria)
-    return resultado;
-}
-
-function calcularSiEsUnNumeroParOImpar(numero){
-    let resultado = ((numero % 2) + 2) %2;
-    console.log(resultado)
-    return resultado;
-}
--- TpIntegradorWeb/js/validacionTarjeta.js
+++ TpIntegradorWeb/js/validacionTarjeta.js
@@ -2,25 +2,25 @@ const regexNroTarjeta = /^[0-9]{10}$/;
 const formularioTarjeta = document.querySelector("#formularioTarjeta");
 const formularioDireccion = document.querySelector("#formularioDireccion");
 
-formularioTarjeta.addEventListener("submit", (e)=>{
+formularioTarjeta.addEventListener("submit", (e) => {
     e.preventDefault();
     validarCamposDelFormulario();
 })
 
-formularioDireccion.addEventListener("submit", (e)=>{
+formularioDireccion.addEventListener("submit", (e) => {
     e.preventDefault();
     validarDatosDireccion();
 })
 
-function validarDatosDireccion(){
+function validarDatosDireccion() {
     let aliasDeAliasDire = document.querySelector("#alias-dire").value;
     let aliasDeDireccion = document.querySelector("#direccion").value;
-    AddDireccion(aliasDeAliasDire,aliasDeDireccion);
+    AddDireccion(aliasDeAliasDire, aliasDeDireccion);
     ListDireccion();
     mostrarDireccion();
 }
 
-function validarCamposDelFormulario(){
+function validarCamposDelFormulario() {
     /* numero de tarjeta no puede todos 0000000000
     el numero de tarjeta debe tener 10 digitos:
     si el ultimo dig es par, la suma de los 9 restantes debe ser impar
@@ -28,10 +28,9 @@ function validarCamposDelFormulario(){
     let error = false;
     let mensajeDeError = "";
     let numeroDeTarjeta = document.querySelector("#nroTarjeta").value;
-    let aliasDeTarjeta = document.querySelector("#alias").value;
     let ultimoDigito = numeroDeTarjeta.charAt(9);
 
-    if(!regexNroTarjeta.test(numeroDeTarjeta)){
+    if (!regexNroTarjeta.test(numeroDeTarjeta)) {
         error = true;
         mensajeDeError += "<p> La tarjeta debe tener 10 digitos </p>"
     }
@@ -39,55 +38,56 @@ function validarCamposDelFormulario(){
     let resultadoDeLaSumatoria = calcularSiLaSumatoriaDeNumerosEsParOImpar(numeroDeTarjeta);
     let resultadoDelUltimoNumero = calcularSiEsUnNumeroParOImpar(ultimoDigito);
 
-    if(resultadoDeLaSumatoria == resultadoDelUltimoNumero){
+    if (resultadoDeLaSumatoria == resultadoDelUltimoNumero) {
         error = true;
         mensajeDeError += "<br><p> Numero de tarjeta inválida</p>"
     }
-    
-    if(numeroDeTarjeta == "0000000000"){
+
+    if (numeroDeTarjeta == "0000000000") {
         error = true;
         mensajeDeError += "<br><p> Numero de tarjeta no puede ser 0000000000</p>"
     }
 
-    if(error){
+    if (error) {
         document.querySelector(".mensajeError").innerHTML = mensajeDeError;
-    } else{
+    } else {
         formularioTarjeta.submit();
+        let aliasDeTarjeta = document.querySelector("#alias").value;
         AddPago(aliasDeTarjeta);
         ListPago();
         mostrarTarjeta();
     }
 }
 
-function calcularSiLaSumatoriaDeNumerosEsParOImpar(numeroDeTarjeta){
+function calcularSiLaSumatoriaDeNumerosEsParOImpar(numeroDeTarjeta) {
     let sumatoria = 0;
-    for(let i = 0; i < numeroDeTarjeta.length; i++){
+    for (let i = 0; i < numeroDeTarjeta.length; i++) {
         sumatoria += parseInt(numeroDeTarjeta.charAt(i))
     }
     let resultado = calcularSiEsUnNumeroParOImpar(sumatoria)
     return resultado;
 }
 
-function calcularSiEsUnNumeroParOImpar(numero){
-    let resultado = ((numero % 2) + 2) %2;
+function calcularSiEsUnNumeroParOImpar(numero) {
+    let resultado = ((numero % 2) + 2) % 2;
     console.log(resultado)
     return resultado;
 }
 
-function mostrarDireccion(){
-    let setPopUpDireccion=document.querySelector("#pop-up-direccion")
+function mostrarDireccion() {
+    let setPopUpDireccion = document.querySelector("#pop-up-direccion")
     estadoDelPopup(setPopUpDireccion)
 }
 
-function mostrarTarjeta(){
-    let setPopUpTarjeta=document.querySelector("#pop-up-tarjeta")
+function mostrarTarjeta() {
+    let setPopUpTarjeta = document.querySelector("#pop-up-tarjeta")
     estadoDelPopup(setPopUpTarjeta)
 }
 
-function estadoDelPopup(popup){
-    if(popup.classList.contains("oculto")){
+function estadoDelPopup(popup) {
+    if (popup.classList.contains("oculto")) {
         popup.classList.remove("oculto")
-    }else{
+    } else {
         popup.classList.add("oculto")
     }
 }
